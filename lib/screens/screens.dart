76e0c26a5019bdd6f3bc07deda70577c1117f472
Samuel Adekunle <oluwatomisinadekunle@gmail.com>
2021-05-03 09:08:12 +0100

    Barrel file for screens folder

---
+++ lib/screens/screens.dart
@@ -0,0 +1,7 @@
+export './confirmation_screen.dart';
+export './dashboard_screen.dart';
+export './country_screen.dart';
+export './login_screen.dart';
+export './splash_screen.dart';
+export './registration_screen.dart';
+export './verification_screen.dart';
